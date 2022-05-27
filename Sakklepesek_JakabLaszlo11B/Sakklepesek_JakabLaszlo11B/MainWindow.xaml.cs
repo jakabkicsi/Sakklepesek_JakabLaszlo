7e21623d2dd7f233cad234c0d5bb3262c7ac2070
jakabkicsi <jakab.kicsi.online@gmail.com>
2022-05-27 19:16:44 +0200

    ComboBox feltöltve
    Feltoltottem a ComboBoxot az elemeivel.

--- Sakklepesek_JakabLaszlo11B/Sakklepesek_JakabLaszlo11B/MainWindow.xaml.cs
+++ Sakklepesek_JakabLaszlo11B/Sakklepesek_JakabLaszlo11B/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,8 @@ namespace Sakklepesek_JakabLaszlo11B
     /// </summary>
     public partial class MainWindow : Window
     {
+        Rectangle[,] tablaMezoi;
+        Grid tabla;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,7 +34,57 @@ namespace Sakklepesek_JakabLaszlo11B
 
         private void FeluletGeneralas()
         {
-            
+            tablaMezoi = new Rectangle[10, 10];
+            char betuJel = 'A';
+            for (int i = 1; i < 9; i++)
+            {
+                Label betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Center;
+                betu.VerticalAlignment = VerticalAlignment.Bottom;
+                betu.Content = betuJel++;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, 0);
+                Grid.SetColumn(betu, i);
+
+                betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Right;
+                betu.VerticalAlignment = VerticalAlignment.Center;
+                betu.Content = betuJel++;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, 9);
+                Grid.SetColumn(betu, i);
+
+                betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Right;
+                betu.VerticalAlignment = VerticalAlignment.Center;
+                betu.Content = 9 - i;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, i);
+                Grid.SetColumn(betu, 9);
+
+                betu = new Label();
+                betu.HorizontalAlignment = HorizontalAlignment.Left;
+                betu.VerticalAlignment = VerticalAlignment.Center;
+                betu.Content = 9 - i;
+                tabla.Children.Add(betu);
+                Grid.SetRow(betu, i);
+                Grid.SetColumn(betu, 9);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    tablaMezoi[i, j] = new Rectangle();
+                    tablaMezoi[i, j].Stroke = Brushes.Black;
+                    tablaMezoi[i, j].Fill = (i + j) % 2 == 0 ? Brushes.White : Brushes.Black;
+                    
+                    tabla.Children.Add(tablaMezoi[i, j]);
+                    Grid.SetColumn(tablaMezoi[i, j], j);
+                    Grid.SetRow(tablaMezoi[i, j], i);
+
+
+                }
+            }
         }
 
         private void ComboBoxFeltoltes()
